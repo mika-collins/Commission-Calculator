dccf76c1933b6ca50681b5442a505cde43b7a778
mika-collins <mikapcollins@gmail.com>
2025-12-16 21:18:37 -0500

    Fix: pylint errors on project skeleton

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,13 @@
+"""
+Backend for Commission Calculator.
+Currently prints a ready message.
+"""
+
 def main():
+    """Print a message indicating the backend is ready."""
     print("Backend is ready!")
+
 
 if __name__ == "__main__":
     main()
+    
